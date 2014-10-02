2baa015321c7a021b46871ac31efdb1a85ee8e09
HugoL <hugoepila@gmail.com>
2014-10-02 19:06:05 +0200

    Subo el esqueleto de la aplicación con un theme

---
+++ themes/abound/views/layouts/tpl_navigation.php
@@ -0,0 +1,28 @@
+<div class="navbar navbar-inverse navbar-fixed-top">
+	<div class="navbar-inner">
+    <div class="container">
+        <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </a>
+     
+          <!-- Be sure to leave the brand out there if you want it shown -->
+          <a class="brand" href="#">Ociopoint <small>Manager</small></a>
+          
+          <div class="nav-collapse">
+			<?php $this->widget('zii.widgets.CMenu',array(
+                    'htmlOptions'=>array('class'=>'pull-right nav'),
+                    'submenuHtmlOptions'=>array('class'=>'dropdown-menu'),
+					'itemCssClass'=>'item-test',
+                    'encodeLabel'=>false,
+                    'items'=>array(
+                        array('label'=>'Mi Perfil', 'url'=>array('/site/index')),                        
+                        array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
+                        array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest),
+                    ),
+                )); ?>
+    	</div>
+    </div>
+	</div>
+</div>
